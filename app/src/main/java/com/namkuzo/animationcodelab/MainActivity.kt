3ddd97b14c53183e5dfc0ed5560a2e826ae50978
paulaarroyo <p.arroyosalvo@gmail.com>
2024-03-29 23:22:08 -0700

    Launch app

--- app/src/main/java/com/namkuzo/animationcodelab/MainActivity.kt
+++ app/src/main/java/com/namkuzo/animationcodelab/MainActivity.kt
@@ -3,44 +3,18 @@ package com.namkuzo.animationcodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.namkuzo.animationcodelab.ui.theme.AnimationCodeLabTheme
+import androidx.activity.enableEdgeToEdge
+import com.namkuzo.animationcodelab.ui.home.Home
+import com.namkuzo.animationcodelab.ui.theme.AnimationCodelabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            AnimationCodeLabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            AnimationCodelabTheme {
+                Home()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnimationCodeLabTheme {
-        Greeting("Android")
     }
 }
